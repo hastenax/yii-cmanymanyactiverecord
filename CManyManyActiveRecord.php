98bb88f9522d39d69261a3c996e34d0e0611a608
unknown <noname@i5.(none)>
2012-07-20 14:53:32 +0400

    Initial commit

---
+++ CManyManyActiveRecord.php
@@ -0,0 +1,131 @@
+<?php
+
+class CManyManyActiveRecord extends CActiveRecord
+{
+		/**
+		* @return array with table name and 2 keys of the related tables
+		*/
+        protected function verifyManyManyRelation($relation) {
+            //check if relation correct
+            if ($this->id < 1)
+                throw new CException($relation->name.' relation error, save model first');
+
+            //check if relation correct
+            if (!is_object($relation) || get_class($relation) != 'CManyManyRelation')
+                throw new CException($relation->name.' is not exist or not belongs to CManyManyRelation class');
+
+            //match tablename(model key, foreign table key)
+            preg_match_all('/([^()]*)\(([^,]*),([^)]*)\)/i', $relation->foreignKey, $matches);
+            return $matches;
+        }
+
+		/**
+		* Create tables relation records on ManyMany relation with deletion old ones
+		* @param string $relationName the name of the relation, needs to be updated
+		* @param array  $relationData array of related keys of second table to be connected with first table
+		*/
+        public function setRelationRecords($relationName, $relationData)
+        {
+            //get correct relation from model relation defenition
+            $relation = $this->getActiveRelation($relationName);
+
+            $matches = $this->verifyManyManyRelation($relation);
+
+            $table = $matches[1][0];
+            $this_key = $matches[2][0];
+            $another_key = $matches[3][0];
+
+            //execute delete old relations statement
+            $sql = "delete from {$table} WHERE $this_key = '{$this->id}'";
+            $command = Yii::app()->db->createCommand($sql);
+            $command->execute();
+
+            //execute insert new relations statement
+            $insert_sql = "insert into {$table} ($this_key, $another_key) VALUES ";
+            $com = Yii::app()->db->createCommand();
+            $c = count($relationData);
+            $sql = array();
+            for ($i = 0; $i<$c; $i++)
+            {
+                $sql[] = '('.$this->id.', '.$relationData[$i].')';
+                //executes insert each 1000 rows or last time
+                if (($i+1 % 1000) == 0 || $i == $c-1)
+                {
+                    $com->setText($insert_sql.implode(', ', $sql));
+                    $com->execute();
+                    $sql = array();
+                }
+            }
+        }
+	
+		/**
+		* Create new tables relation records on ManyMany relation without deletion old ones
+		* @param string    $relationName the name of the relation, needs to be updated
+		* @param array	   $relationData array of related keys of second table to be connected with first table
+		*/
+        public function addRelationRecords($relationName, $relationData, $additionalField = array())
+        {
+            //get correct relation from model relation defenition
+            $relation = $this->getActiveRelation($relationName);
+
+            $matches = $this->verifyManyManyRelation($relation);
+
+            $table = $matches[1][0];
+            $this_key = $matches[2][0];
+            $another_key = $matches[3][0];
+
+            //execute insert new relations statement
+            if (count($additionalField) > 0) {
+		foreach($additionalField as $key=>$value) {
+		    $keys[] = $key;
+		}
+                $insert_sql = "insert into {$table} ($this_key, $another_key, ".implode(',', $keys).") VALUES ";
+	    }
+            else
+                $insert_sql = "insert into {$table} ($this_key, $another_key) VALUES ";
+            $com = Yii::app()->db->createCommand();
+            $c = count($relationData);
+            $sql = array();
+            for ($i = 0; $i<$c; $i++)
+            {
+                if (count($additionalField) > 0) {
+		    foreach($additionalField as $key=>$value) {
+			$values[] = $value;
+		    }
+                    $sql[] = '('.$this->id.', '.$relationData[$i].", '".implode("', '", $values)."')";
+		}
+                else
+                    $sql[] = '('.$this->id.', '.$relationData[$i].')';
+                //executes insert each 1000 rows or last time
+                if (($i+1 % 1000) == 0 || $i == $c-1)
+                {
+                    $com->setText($insert_sql.implode(', ', $sql));
+                    $com->execute();
+                    $sql = array();
+                }
+            }
+        }
+
+        /**
+		* Remove tables relation records on ManyMany relation
+		* @param string    $relationName the name of the relation, needs to be updated
+		* @param int	   $keys array of keys to remove
+		*/
+        public function removeRelationRecords($relationName, $keys)
+        {
+            //get correct relation from model relation defenition
+            $relation = $this->getActiveRelation($relationName);
+
+            $matches = $this->verifyManyManyRelation($relation);
+
+            $table = $matches[1][0];
+            $this_key = $matches[2][0];
+            $another_key = $matches[3][0];
+
+            //execute delete relation statement
+            $sql = "delete from {$table} WHERE $this_key = '{$this->id}' AND $another_key IN (".implode(',', $keys).")";
+            $command = Yii::app()->db->createCommand($sql);
+            $command->execute();
+        }	
+}
+?>
--- README.md
+++ README.md
@@ -1,4 +1,47 @@
 yii-cmanymanyactiverecord
 =========================
 
-CManyManyActiveRecord class for simple and quick saving MANY_MANY relation data in models
+The CManyManyActiveRecord YII extension class adds up functionality of saving MANY_MANY relation field value using simple arrays.
+
+Requirements 
+=========================
+
+Yii 1.1 or above
+
+Installation
+========================= 
+
+To use this extension:
+1) copy CManyManyActiveRecord to your components directory
+2) check that your config have autoloaded
+	'import'=>array(
+		...
+		'application.components.*',
+		...
+3) Extend your model (which has MANY_MANY relation), category for example
+	class Category extends CManyManyActiveRecord
+	...
+
+Usage
+=========================
+
+If Category has: 'posts'=>array(self::MANY_MANY, 'Post', 'tbl_post_category(category_id, post_id)')
+
+Create tbl_post_category (category_id, post_id) table and then
+
+you can set realtions by (with erasing old ones)
+
+	$model = Category::model()->findByPk(10);
+	$model->setRelationRecords('posts',array(1, 2, 3));
+
+or you can add new relations (without deletions of old ones)
+
+	$model = Category::model()->findByPk(10);
+	$model->addRelationRecords('posts',array(1, 2, 3));
+
+or you can add remove some raltions
+
+	$model = Category::model()->findByPk(10);
+	$model->removeRelationRecords('posts',array(1,2,3));
+
+Each of this method saves data to database, you don't need to save the model.
