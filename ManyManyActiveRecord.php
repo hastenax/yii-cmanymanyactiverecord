a3da45c728eb252974e216d68c95b96b6ba99dc8
hastenax <hastenax@yandex.ru>
2012-08-17 09:46:53 +0400

    Update ManyManyActiveRecord.php
    + Fixed issue #1.
    + Added transaction support for setRelationRecords.

--- ManyManyActiveRecord.php
+++ ManyManyActiveRecord.php
@@ -6,8 +6,11 @@ class ManyManyActiveRecord extends CActiveRecord
 	* @return array with table name and 2 keys of the related tables
 	*/
         protected function verifyManyManyRelation($relation) {
+            //check if primaryKey correct
+            if (is_array($this->primaryKey))
+                throw new CException('ManyManyActiveRecord can\'t work  with composite primary key');	    
             //check if relation correct
-            if ($this->id < 1)
+            if ($this->primaryKey < 1)
                 throw new CException($relation->name.' relation error, save model first');
 
             //check if relation correct
@@ -24,7 +27,7 @@ class ManyManyActiveRecord extends CActiveRecord
 	* @param string $relationName the name of the relation, needs to be updated
 	* @param array  $relationData array of related keys of second table to be connected with first table
 	*/
-        public function setRelationRecords($relationName, $relationData)
+        public function setRelationRecords($relationName, $relationData, $additionalFields = array(), $useTransaction = false)
         {
             //get correct relation from model relation defenition
             $relation = $this->getActiveRelation($relationName);
@@ -34,34 +37,59 @@ class ManyManyActiveRecord extends CActiveRecord
             $table = $matches[1][0];
             $this_key = $matches[2][0];
             $another_key = $matches[3][0];
+            
+            if ($useTransaction)
+                $transaction = Yii::app()->db->beginTransaction();
 
-            //execute delete old relations statement
-            $sql = "delete from {$table} WHERE $this_key = '{$this->id}'";
-            $command = Yii::app()->db->createCommand($sql);
-            $command->execute();
+            try {
+                //execute delete old relations statement
+                $sql = "delete from {$table} WHERE $this_key = '{$this->primaryKey}'";
+                $command = Yii::app()->db->createCommand($sql);
+                $command->execute();
 
-            //execute insert new relations statement
-            $insert_sql = "insert into {$table} ($this_key, $another_key) VALUES ";
-            $com = Yii::app()->db->createCommand();
-            $c = count($relationData);
-            $sql = array();
-            for ($i = 0; $i<$c; $i++)
-            {
-                $sql[] = '('.$this->id.', '.$relationData[$i].')';
-                //executes insert each 1000 rows or last time
-                if (($i+1 % 1000) == 0 || $i == $c-1)
-                {
-                    $com->setText($insert_sql.implode(', ', $sql));
-                    $com->execute();
-                    $sql = array();
+                //execute insert new relations statement
+                if (count($additionalFields) > 0) {
+                    foreach($additionalFields as $key=>$value) {
+                        $keys[] = $key;
+                    }
+                    $insert_sql = "insert into {$table} ($this_key, $another_key, ".implode(',', $keys).") VALUES ";
                 }
+                else
+                    $insert_sql = "insert into {$table} ($this_key, $another_key) VALUES ";
+                $com = Yii::app()->db->createCommand();
+                $c = count($relationData);
+                $sql = array();
+                for ($i = 0; $i<$c; $i++) {
+                    if (count($additionalFields) > 0) {
+                        foreach($additionalFields as $key=>$value) {
+                            $values[] = $value;
+                        }
+                        $sql[] = '('.$this->primaryKey.', '.$relationData[$i].", '".implode("', '", $values)."')";
+                    }
+                    else
+                         $sql[] = '('.$this->primaryKey.', '.$relationData[$i].')';
+                    //executes insert each 1000 rows or last time
+                    if (($i+1 % 1000) == 0 || $i == $c-1) {
+                        $com->setText($insert_sql.implode(', ', $sql));
+                        $com->execute();
+                        $com = Yii::app()->db->createCommand();
+                        $sql = array();
+                    }
+                }
+                if ($useTransaction)
+                    $transaction->commit();
+            }
+            catch(Exception $e) {
+                if ($useTransaction)
+                    $transaction->rollback();
+                throw new CException($e->getMessage());
             }
         }
 	
 	/**
 	* Create new tables relation records on ManyMany relation without deletion old ones
-	* @param string    $relationName the name of the relation, needs to be updated
-	* @param array	   $relationData array of related keys of second table to be connected with first table
+	* @param string     $relationName the name of the relation, needs to be updated
+	* @param array	    $relationData array of related keys of second table to be connected with first table
 	*/
         public function addRelationRecords($relationName, $relationData, $additionalFields = array())
         {
@@ -86,19 +114,17 @@ class ManyManyActiveRecord extends CActiveRecord
             $com = Yii::app()->db->createCommand();
             $c = count($relationData);
             $sql = array();
-            for ($i = 0; $i<$c; $i++)
-            {
+            for ($i = 0; $i<$c; $i++) {
                 if (count($additionalFields) > 0) {
 		    foreach($additionalFields as $key=>$value) {
 			$values[] = $value;
 		    }
-                    $sql[] = '('.$this->id.', '.$relationData[$i].", '".implode("', '", $values)."')";
+                    $sql[] = '('.$this->primaryKey.', '.$relationData[$i].", '".implode("', '", $values)."')";
 		}
                 else
-                    $sql[] = '('.$this->id.', '.$relationData[$i].')';
+                    $sql[] = '('.$this->primaryKey.', '.$relationData[$i].')';
                 //executes insert each 1000 rows or last time
-                if (($i+1 % 1000) == 0 || $i == $c-1)
-                {
+                if (($i+1 % 1000) == 0 || $i == $c-1) {
                     $com->setText($insert_sql.implode(', ', $sql));
                     $com->execute();
                     $sql = array();
@@ -108,8 +134,8 @@ class ManyManyActiveRecord extends CActiveRecord
 
         /**
 	* Remove tables relation records on ManyMany relation
-	* @param string    $relationName the name of the relation, needs to be updated
-	* @param int	   $keys array of keys to remove
+	* @param string $relationName the name of the relation, needs to be updated
+	* @param int	$keys array of keys to remove
 	*/
         public function removeRelationRecords($relationName, $keys)
         {
@@ -123,7 +149,7 @@ class ManyManyActiveRecord extends CActiveRecord
             $another_key = $matches[3][0];
 
             //execute delete relation statement
-            $sql = "delete from {$table} WHERE $this_key = '{$this->id}' AND $another_key IN (".implode(',', $keys).")";
+            $sql = "delete from {$table} WHERE $this_key = '{$this->primaryKey}' AND $another_key IN (".implode(',', $keys).")";
             $command = Yii::app()->db->createCommand($sql);
             $command->execute();
         }	
